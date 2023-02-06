ff9b5f10e185645891a2de96d4662f18bedac932
Владислав Пашковский <sparoW2021@yandex.ru>
2023-02-07 02:08:31 +0300

    props_c2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import PostItem from "./components/PostItem";
 import './styles/App.css'
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body:'Description'},
+    {id: 2, title: 'Typescript', body:'Description'},
+    {id: 3, title: 'NodeJS', body:'Description'},
+    {id: 4, title: 'React', body:'Description'}
+  ])
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'Javascript', body:'Description'}}/>
-      <PostItem post={{id: 2, title: 'Typescript', body:'Description'}}/>
-
+      {posts.map(post => 
+        <PostItem post={post}/>
+      )}
     </div>
   );
 }
