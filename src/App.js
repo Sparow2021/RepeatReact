c6f89558ec0e95aec4f7d1c45f09d223884f47e7
Владислав Пашковский <sparoW2021@yandex.ru>
2023-02-08 01:08:54 +0300

    Add new post logic

--- src/App.js
+++ src/App.js
@@ -21,17 +21,21 @@ function App() {
 
   function addNewPost(event) {
     event.preventDefault();
-    console.log({ title })
-    console.log({ body })
-
-    console.log('Новый пост добавлен!')
+    const newPost = {
+      id: Date.now(),
+      title,
+      body
+    }
+    setPosts([...posts, newPost]);
+    setTitle('');
+    setBody('');
   }
 
   return (
     <div className="App">
       <form>
         <MyInput type="text" placeholder="Название" value={title} onChange={e => setTitle(e.target.value)} />
-        <MyInput type="text" placeholder="Описание" value={body} onChange={e=>setBody(e.target.value)} />
+        <MyInput type="text" placeholder="Описание" value={body} onChange={e => setBody(e.target.value)} />
         <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title={'Список курсов: '} />
