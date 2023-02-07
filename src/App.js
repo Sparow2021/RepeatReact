da972104abd5cc8076a5deec0228294788f95cda
Владислав Пашковский <sparoW2021@yandex.ru>
2023-02-08 00:41:29 +0300

    ref logic bad for DOM in REACT

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
@@ -15,10 +15,12 @@ function App() {
   ])
 
   const [title, setTitle] = useState('sadasd');
+  const bodyInputRef = useRef();
 
   function addNewPost(event){
     event.preventDefault();
     console.log({title})
+    console.log(bodyInputRef.current.value)
     console.log('Новый пост добавлен!')
   }
 
@@ -26,7 +28,8 @@ function App() {
     <div className="App">
       <form>
         <MyInput type="text" placeholder="Название" value={title} onChange={e => setTitle(e.target.value)}/>
-        <MyInput type="text" placeholder="Описание"/>
+        <input ref={bodyInputRef} type="text" placeholder="test"/>
+        {/* <MyInput type="text" placeholder="Описание"/> */}
         <MyButton  onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title={'Список курсов: '}/>
